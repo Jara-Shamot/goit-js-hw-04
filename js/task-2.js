f0639ae6c1e820009efd92161c243672c5a0d96c
Jara-Shamot <141449562+Jara-Shamot@users.noreply.github.com>
2023-12-14 19:22:48 +0100

    add all tasks

---
+++ js/task-2.js
@@ -0,0 +1,38 @@
+'use strick';
+//Задача 2. Розрахунок калорій//
+
+function calcAverageCalories(days) {
+  let sum = 0;
+  let count = 0;
+  for (let dayRecord of days) {
+    sum += dayRecord.calories;
+    count += 1;
+  }
+  return count > 0 ? Math.round(sum / count) : 0;
+}
+
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 3010 },
+    { day: 'tuesday', calories: 3200 },
+    { day: 'wednesday', calories: 3120 },
+    { day: 'thursday', calories: 2900 },
+    { day: 'friday', calories: 3450 },
+    { day: 'saturday', calories: 3280 },
+    { day: 'sunday', calories: 3300 },
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 2040 },
+    { day: 'tuesday', calories: 2270 },
+    { day: 'wednesday', calories: 2420 },
+    { day: 'thursday', calories: 1900 },
+    { day: 'friday', calories: 2370 },
+    { day: 'saturday', calories: 2280 },
+    { day: 'sunday', calories: 2610 },
+  ])
+); // 2270
+
+console.log(calcAverageCalories([])); // 0
